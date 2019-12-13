a222c030c5f18fecfe724ce11770109454dffd56
Arpan Banerjee <arpanbnrj9@gmail.com>
2019-12-13 03:56:03 -0500

    changed webpack config

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -13,10 +13,12 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+    'app': glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
+    'login': glob.sync('./vendor/**/*.js').concat(['./js/login.js']),
+    'homepage': glob.sync('./vendor/**/*.js').concat(['./js/homepage.js'])
   },
   output: {
-    filename: 'app.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, '../priv/static/js')
   },
   module: {
@@ -35,7 +37,7 @@ module.exports = (env, options) => ({
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new MiniCssExtractPlugin({ filename: '../css/[name].css' }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });
